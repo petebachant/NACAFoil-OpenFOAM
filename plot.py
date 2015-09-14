e5ec9eee44c64d2f7b7c110dd65d8266bbf075cf
Pete Bachant <petebachant@gmail.com>
2015-09-14 16:59:20 -0400

    Add ability to plot time series; resolves #15

--- plot.py
+++ plot.py
@@ -8,9 +8,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
+import modules.processing as proc
 
 ylabels = {"cl": r"$C_l$", "cd": r"$C_d$", "cl/cd": r"$C_l/C_d$", "k": "$k$",
            "omega": r"$\omega$", "epsilon": r"$\epsilon$"}
+
+
+def plot_time_series(quantity="cl"):
+    """
+    Plot specified quantity over time. Can be used to visualize convergence.
+    """
+    df = proc.load_force_coeffs()
+    if quantity == "cl/cd":
+        q = df.cl/df.cd
+    else:
+        q = df[quantity]
+    plt.figure()
+    plt.plot(df.time[5:], q[5:])
+    plt.xlabel(r"$t$")
+    plt.ylabel(ylabels[quantity])
+    plt.grid(True)
+    plt.tight_layout()
+
 
 def plot_foil_perf(quantity="cl/cd", foil="0012", Re=2e5):
     df = pd.read_csv("processed/NACA{}_{:.1e}.csv".format(foil, Re))
@@ -25,6 +44,7 @@ def plot_foil_perf(quantity="cl/cd", foil="0012", Re=2e5):
     plt.grid(True)
     plt.tight_layout()
 
+
 if __name__ == "__main__":
     try:
         import seaborn
@@ -33,24 +53,29 @@ if __name__ == "__main__":
         print("Could not import seaborn for plot styling. Try")
         print("\n    conda install seaborn\n\nor")
         print("\n    pip install seaborn\n")
-        
+
     parser = argparse.ArgumentParser(description="Plotting results")
-    parser.add_argument("quantity", nargs="?", default="cl/cd", 
-                        help="Which quantity to plot", 
+    parser.add_argument("quantity", nargs="?", default="cl/cd",
+                        help="Which quantity to plot",
                         choices=["cl", "cd", "cl/cd", "k", "omega", "epsilon"])
     parser.add_argument("--foil", "-f", help="Foil", default="0012")
     parser.add_argument("--Reynolds", "-R", help="Reynolds number", default=2e5)
     parser.add_argument("--save", "-s", action="store_true", help="Save plots")
-    parser.add_argument("--noshow", action="store_true", default=False, 
+    parser.add_argument("--noshow", action="store_true", default=False,
                         help="Do not show")
+    parser.add_argument("--timeseries", "-t", action="store_true",
+                        default=False)
     args = parser.parse_args()
 
-    plot_foil_perf(args.quantity, args.foil, float(args.Reynolds))
-    
+    if args.timeseries:
+        plot_time_series(args.quantity)
+    else:
+        plot_foil_perf(args.quantity, args.foil, float(args.Reynolds))
+
     if args.save:
         if not os.path.isdir("figures"):
             os.mkdir("figures")
         plt.savefig("figures/{}.pdf".format(args.quantity))
-        
+
     if not args.noshow:
         plt.show()
