265924a8e673191d7875bbbf8cc4179179ff89f6
Pete Bachant <petebachant@gmail.com>
2015-10-24 11:18:10 -0400

    Force coeffs are loaded for all time; resolves #16

--- modules/processing.py
+++ modules/processing.py
@@ -6,16 +6,9 @@ import numpy as np
 import pandas as pd
 import os
 
-def load_force_coeffs(steady=False):
-    """
-    Load force coefficients from file. If steady, the file from the `0`
-    directory is used, and the last values are returned. Otherwise, arrays are
-    loaded from the latest file.
-    """
-    if steady:
-        timedir = "0"
-    else:
-        timedir = max(os.listdir("postProcessing/forceCoeffs"))
+
+def load_force_coeffs_single(timedir):
+    """Load force coefficients into a DataFrame for the given time directory."""
     fpath = "postProcessing/forceCoeffs/{}/forceCoeffs.dat".format(timedir)
     data = np.loadtxt(fpath, skiprows=9)
     df = pd.DataFrame()
@@ -24,3 +17,19 @@ def load_force_coeffs(steady=False):
     df["cd"] = data[:, 2]
     df["cm"] = data[:, 1]
     return df
+
+
+def load_force_coeffs(steady=False):
+    """
+    Load force coefficients from file. If steady, the file from the `0`
+    directory is used, and the last values are returned. Otherwise, arrays are
+    loaded from the latest file.
+    """
+    if steady:
+        return load_force_coeffs_single("0")
+    else:
+        df = pd.DataFrame()
+        timedirs = sorted(os.listdir("postProcessing/forceCoeffs"))
+        for t in timedirs:
+            df = df.append(load_force_coeffs_single(t), ignore_index=True)
+    return df.sort("time")
