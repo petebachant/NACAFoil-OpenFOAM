faf43c3e3d8b8e1c2c6837d4fa36cb28719c8c55
Pete Bachant <petebachant@gmail.com>
2025-01-07 18:30:16 -0800

    Reformat plot.py

--- plot.py
+++ plot.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python
-"""
-This script plots various quantities.
-"""
+"""This script plots various quantities."""
 
-from __future__ import division, print_function
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import argparse
-import pynfof.processing as proc
 import os
 
-labels = {"cl": r"$C_l$", "cd": r"$C_d$", "cl/cd": r"$C_l/C_d$",
-          "k": r"$k/U_\infty^2$", "omega": r"$\omega$", "epsilon":
-          r"$\epsilon$", "alpha_deg": r"$\alpha$ (deg)", "cm": r"$C_m$",
-          "cn": "$C_n$", "cc": "$C_c$"}
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+import pynfof.processing as proc
+
+labels = {
+    "cl": r"$C_l$",
+    "cd": r"$C_d$",
+    "cl/cd": r"$C_l/C_d$",
+    "k": r"$k/U_\infty^2$",
+    "omega": r"$\omega$",
+    "epsilon": r"$\epsilon$",
+    "alpha_deg": r"$\alpha$ (deg)",
+    "cm": r"$C_m$",
+    "cn": "$C_n$",
+    "cc": "$C_c$",
+}
 
 
 def plot_time_series(quantity="cl"):
-    """
-    Plot specified quantity over time. Can be used to visualize convergence.
+    """Plot specified quantity over time.
+
+    Can be used to visualize convergence.
     """
     df = proc.load_force_coeffs()
     if quantity == "cl/cd":
-        q = df.cl/df.cd
+        q = df.cl / df.cd
     else:
         q = df[quantity]
     plt.figure()
@@ -34,16 +42,17 @@ def plot_time_series(quantity="cl"):
     plt.tight_layout()
 
 
-def plot_foil_perf(quantity="cl/cd", foil="0012", Re=2e5, x="alpha_deg",
-                   ax=None, marker="-o"):
+def plot_foil_perf(
+    quantity="cl/cd", foil="0012", Re=2e5, x="alpha_deg", ax=None, marker="-o"
+):
     df = pd.read_csv("processed/NACA{}_{:.1e}.csv".format(foil, Re))
     alpha = np.deg2rad(df.alpha_deg)
-    df["cn"] = df.cl*np.cos(alpha) - df.cd*np.sin(alpha)
-    df["cc"] = df.cl*np.sin(alpha) - df.cd*np.cos(alpha)
+    df["cn"] = df.cl * np.cos(alpha) - df.cd * np.sin(alpha)
+    df["cc"] = df.cl * np.sin(alpha) - df.cd * np.cos(alpha)
     if ax is None:
         fig, ax = plt.subplots()
     if quantity == "cl/cd":
-        q = df.cl/df.cd
+        q = df.cl / df.cd
     else:
         q = df[quantity]
     ax.plot(df[x], q, marker, label="NACA " + foil)
@@ -56,18 +65,26 @@ def plot_foil_perf(quantity="cl/cd", foil="0012", Re=2e5, x="alpha_deg",
         pass
 
 
-def plot_multiple_foils(quantity="cl/cd", foils=["0012", "0021"], Re=2e5,
-                        x="alpha_deg", save=False):
+def plot_multiple_foils(
+    quantity="cl/cd", foils=["0012", "0021"], Re=2e5, x="alpha_deg", save=False
+):
     """Plot performance for multiple foils."""
     fig, ax = plt.subplots()
     for foil, m in zip(foils, ("-o", "--^", "-s", "--v")):
-        plot_foil_perf(quantity=quantity, foil=foil, Re=Re, x=x, ax=ax,
-                       marker=m)
+        plot_foil_perf(
+            quantity=quantity, foil=foil, Re=Re, x=x, ax=ax, marker=m
+        )
     ax.legend(loc="best")
     fig.tight_layout()
     if save:
-        figname = "NACA-" + "-".join(foils) + "-" + quantity.replace("/", "") \
-                + "-vs-" + x
+        figname = (
+            "NACA-"
+            + "-".join(foils)
+            + "-"
+            + quantity.replace("/", "")
+            + "-vs-"
+            + x
+        )
         fig.savefig("figures/" + figname + ".pdf")
         fig.savefig("figures/" + figname + ".png", dpi=300)
 
@@ -75,28 +92,58 @@ def plot_multiple_foils(quantity="cl/cd", foils=["0012", "0021"], Re=2e5,
 if __name__ == "__main__":
     try:
         import seaborn
-        seaborn.set(style="white", context="notebook", font_scale=1.5,
-                    rc={"axes.grid": True, "legend.frameon": True,
-                        "lines.markeredgewidth": 1.4, "lines.markersize": 10})
+
+        seaborn.set(
+            style="white",
+            context="notebook",
+            font_scale=1.5,
+            rc={
+                "axes.grid": True,
+                "legend.frameon": True,
+                "lines.markeredgewidth": 1.4,
+                "lines.markersize": 10,
+            },
+        )
     except ImportError:
         print("Could not import seaborn for plot styling. Try")
         print("\n    conda install seaborn\n\nor")
         print("\n    pip install seaborn\n")
 
     parser = argparse.ArgumentParser(description="Plotting results")
-    parser.add_argument("quantity", nargs="?", default="cl/cd",
-                        help="Which quantity to plot",
-                        choices=["cl", "cd", "cm", "cl/cd", "k", "omega",
-                                 "epsilon", "cn", "cc"])
+    parser.add_argument(
+        "quantity",
+        nargs="?",
+        default="cl/cd",
+        help="Which quantity to plot",
+        choices=[
+            "cl",
+            "cd",
+            "cm",
+            "cl/cd",
+            "k",
+            "omega",
+            "epsilon",
+            "cn",
+            "cc",
+        ],
+    )
     parser.add_argument("-x", help="Quantity on x-axis", default="alpha_deg")
     parser.add_argument("--foil", "-f", help="Foil", default="0012")
     parser.add_argument("--foils", "-F", help="Multiple foils", nargs="*")
-    parser.add_argument("--Reynolds", "-R", help="Reynolds number", default=2e5)
+    parser.add_argument(
+        "--Reynolds", "-R", help="Reynolds number", default=2e5
+    )
     parser.add_argument("--save", "-s", action="store_true", help="Save plots")
-    parser.add_argument("--noshow", action="store_true", default=False,
-                        help="Do not show")
-    parser.add_argument("--timeseries", "-t", action="store_true",
-                        default=False, help="Plot time series data")
+    parser.add_argument(
+        "--noshow", action="store_true", default=False, help="Do not show"
+    )
+    parser.add_argument(
+        "--timeseries",
+        "-t",
+        action="store_true",
+        default=False,
+        help="Plot time series data",
+    )
     args = parser.parse_args()
 
     if args.save:
@@ -106,11 +153,17 @@ if __name__ == "__main__":
     if args.timeseries:
         plot_time_series(args.quantity)
     elif args.foils:
-        plot_multiple_foils(args.quantity, args.foils, float(args.Reynolds),
-                            x=args.x, save=args.save)
+        plot_multiple_foils(
+            args.quantity,
+            args.foils,
+            float(args.Reynolds),
+            x=args.x,
+            save=args.save,
+        )
     else:
-        plot_foil_perf(args.quantity, args.foil, float(args.Reynolds),
-                       x=args.x)
+        plot_foil_perf(
+            args.quantity, args.foil, float(args.Reynolds), x=args.x
+        )
 
     if not args.noshow:
         plt.show()
